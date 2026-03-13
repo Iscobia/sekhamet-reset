cd489d227c2db18a98aefa685f273cfe9fbc85a6
Iscobia <28196002+Iscobia@users.noreply.github.com>
2026-03-13 21:11:02 +0100

    ajout du flow d'apprentissage automatique

--- config.js
+++ config.js
@@ -22,6 +22,8 @@ window.NOTIFICATION_APP_IDS = window.ALLOWED_APP_IDS;
 window.DEFAULT_APP_ID = "reset";
 window.TECH_SUPPORT_EMAIL = "contact@sekhamet.com";
 
+// Ordre de progression par défault
+window.PROGRAM_FLOW = ["reset]
 
 // OneSignal : laisser sur false tant que tu veux seulement les notifs natives
 window.ENABLE_ONESIGNAL = false;
